b5960689c9ae333589c7b5f4afd73723e1df1ba8
Saksham <sakshamiitroorkee3@gmail.com>
2025-02-22 23:55:48 +0530

    Improved instruction_memory module. Removed clk, chip_select_i and reset signal. As I found them unusable for this implementation.

--- RISCV-SINGLE-CYCLE/Components/instruction_memory.v
+++ RISCV-SINGLE-CYCLE/Components/instruction_memory.v
@@ -1,32 +1,14 @@
 module instruction_memory(
-    input wire clk,
-    input wire reset,
-    input wire chip_select_i,
     input wire [31:0] address,
     output reg [31:0] instruction
 );
-    
         reg [31:0] mem[0:1023];
-        integer i;
         initial 
             begin
                 $readmemh("E:/Verilog/IOP/instruction.mif",mem);
             end
-        always@(posedge clk)
+        always@(address)
             begin
-                if(chip_select_i == 1'b1 && reset==1'b1)
-                    begin
-                        for(i=0;i<1024;i=i+1)
-                            mem[i] <= 32'b0;
-                    end
-            end
-        always@(chip_select_i,address)
-            begin
-                if(chip_select_i == 1'b1)
-                    begin
-                        instruction <= mem[address[31:2]]; // Word Addressable Memory as Instruction are of word size
-                    end
-                else 
-                    instruction <= 32'bz;    
+                        instruction <= mem[address[31:2]]; // Word Addressable Memory as Instruction are of word size  
             end    
 endmodule
